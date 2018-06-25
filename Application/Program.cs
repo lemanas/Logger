8fcac29e24cc52cd12b048a0d24577bcc5978313
Vilius Lastauskas <vilius.lastauskas@euromonitor.com>
2018-06-25 17:11:45 +0300

    reinit

---
+++ Application/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using LogTest;
+
+namespace LogUsers
+{
+    class Program
+    {
+        static void Main()
+        {
+            ILog  logger = new AsyncLog();
+
+            for (int i = 0; i < 15; i++)
+            {
+                logger.Write("Number with Flush: " + i + "\n");
+                Thread.Sleep(50);
+            }
+
+            //logger.StopWithFlush();
+
+            ILog logger2 = new AsyncLog();
+
+            for (int i = 50; i > 0; i--)
+            {
+                logger2.Write("Number with No flush: " + i + "\n");
+                Thread.Sleep(20);
+            }
+
+            //logger2.StopWithoutFlush();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogTest/AsyncLog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogTest
+{
+    public class AsyncLog : ILog
+    {
+        private readonly string _path; 
+
+        public AsyncLog()
+        {
+            if (!Directory.Exists(@"C:\LogTest")) 
+                Directory.CreateDirectory(@"C:\LogTest");
+
+            _path = @"C:\LogTest\Log" + DateTime.Today.ToString("yyyyMMdd") + ".log";
+        }
+
+        public void StopWithoutFlush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopWithFlush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Write(string text)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                byte[] encodedText = Encoding.Unicode.GetBytes(text);
+                byte[] newLine = Encoding.Unicode.GetBytes(Environment.NewLine);
+
+                using (var sourceStream = new FileStream(_path, FileMode.Append, FileAccess.Write, FileShare.None, 4096, true))
+                {
+                    sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
+                    sourceStream.WriteAsync(newLine, 0, newLine.Length);
+                }
+            });
+        }
+    }
+}
